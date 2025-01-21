844735896bde6a5b487a6c122807bf1b72112292
Gautam Kumar <gautam14cs@gmail.com>
2025-01-21 22:47:35 +0530

    JDK-8 Initial commit

--- Core-Java/JDK-1.8/src/com/gautam/general/Jdk8.java
+++ Core-Java/JDK-1.8/src/com/gautam/general/Jdk8.java
@@ -1,8 +1,8 @@
 package com.gautam.general;
 
 public class Jdk8 {
- public static void main(String[] args) {
-	
-	 System.out.println(" JDK 8 Feature !!!");
-}
+	public static void main(String[] args) {
+
+		System.out.println(" JDK 8 Feature !!!");
+	}
 }
